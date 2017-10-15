9d63f3b4525c4a2f9f6d3552f7d5b28dc948866d
June <maplev@163.com>
2017-10-15 21:34:25 +0800

    some modification

--- grpc-build/build.sh
+++ grpc-build/build.sh
@@ -4,15 +4,16 @@ yum groupinstall -y "Development Tools"
 
 # install protobuf
 cd /var/local/git/grpc/third_party/protobuf && \
-./autogen.sh && ./configure && make && make install && ldconfig && which protoc && protoc --version
+make clean && ./autogen.sh && ./configure && make && make install && ldconfig && which protoc && protoc --version
 
 # install grpc
 cd /var/local/git/grpc && \
-make && make install
+make clean && make && make install
 
 # compile grpc cpp helloword
 cd /var/local/git/grpc && \
-cp /var/local/git/grpc-build/activate.sh . && \
+rm -f activate.sh && \
+cp -f /var/local/git/grpc-build/activate.sh . && \
 source activate.sh && \
 cd examples/cpp/helloworld && \
 make
